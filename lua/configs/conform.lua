170a84dc344142f7728527f379e4ffab7f39980d
Sébastien Cuthbert <sebastiencuthbert@Sebastiens-MacBook-Pro.local>
2025-05-30 12:54:00 +0200

    update files

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,40 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+    },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+    formatters = {
+        -- -- C & C++
+        ["clang-format"] = {
+            prepend_args = {
+                "-style={ \
+                        IndentWidth: 4, \
+                        TabWidth: 4, \
+                        UseTab: Never, \
+                        AccessModifierOffset: 0, \
+                        IndentAccessModifiers: true, \
+                        PackConstructorInitializers: Never}",
+            },
+        },
+        -- -- Lua
+        -- stylua = {
+        --     prepend_args = {
+        --         "--column-width", "80",
+        --         "--line-endings", "Unix",
+        --         "--indent-type", "Spaces",
+        --         "--indent-width", "4",
+        --         "--quote-style", "AutoPreferDouble",
+        --     },
+        -- },
+    },
+
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 }
 
-return options
+require("conform").setup(options)
